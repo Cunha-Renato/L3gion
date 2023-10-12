eb92b81d897596a11ed74fa559f2eacaf3b64a5b
Renato M. Cunha <cunhamrenato@gmail.com>
2023-10-12 08:09:13 -0300

    Entry Point

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,19 @@
+#include <L3gion.h>
+
+class Sandbox : public L3gion::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+	~Sandbox()
+	{
+
+	}
+};
+
+L3gion::Application* L3gion::createApplication()
+{
+	return new Sandbox();
+}
